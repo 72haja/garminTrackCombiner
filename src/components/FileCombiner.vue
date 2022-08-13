8cb51468ecf64409e007b73b5c127ebbc8ee57ae
Jakob Haag <jakob.haag@gmx.de>
2022-08-13 11:51:55 +0200

    combine Laps not tracks

--- src/components/FileCombiner.vue
+++ src/components/FileCombiner.vue
@@ -8,14 +8,7 @@ const localFiles = ref([])
 const combinedFile = ref("")
 const combining = ref(false)
 
-const xmlCombinedValues = ref({
-  totalTimeSeconds: 0,
-  distanceMeters: 0,
-  maximumSpeed: 0,
-  calories: 0,
-  maximumHeartRateBpm: 0,
-  trackPoints: [],
-})
+const allLabs = ref([])
 
 function updateLocalFiles (files) {
   files.forEach((file) => {
@@ -35,56 +28,43 @@ function updateLocalFiles (files) {
 function combineFiles () {
   combining.value = true;
 
-  resetXmlCombinedValues();
+  resetLaps();
 
   const xmlDocs = getXmlDocs()
 
   xmlDocs.forEach((doc) => {
-    setXmlCombinedValues(doc)
+    setLaps(doc)
   })
+
+  sortLapsByXmlStartTimeAttribute();
 
   setXmlCombinedFileValues()
   combining.value = false;
 }
 
-function resetXmlCombinedValues () {
-  xmlCombinedValues.value = {
-    totalTimeSeconds: 0,
-    distanceMeters: 0,
-    maximumSpeed: 0,
-    calories: 0,
-    maximumHeartRateBpm: 0,
-    trackPoints: [],
-  }
+function resetLaps () {
+  allLabs.value = []
 }
 
 function getXmlDocs () {
   return localFiles.value.map((file) => {
     const parser = new DOMParser();
     return parser.parseFromString(file.content, "text/xml");
-  }).filter((doc) => doc.documentElement.nodeName !== "parsererror")
+  })
 }
 
-function setXmlCombinedValues (doc) {
-  const docTotalTimeSeconds = doc.getElementsByTagName("TotalTimeSeconds")[0].childNodes[0].nodeValue;
-  const docDistanceMeters = doc.getElementsByTagName("DistanceMeters")[0].childNodes[0].nodeValue;
-  const docMaximumSpeed = doc.getElementsByTagName("MaximumSpeed")[0].childNodes[0].nodeValue;
-  const docCalories = doc.getElementsByTagName("Calories")[0].childNodes[0].nodeValue;
-  const docMaximumHeartRateBpm = doc.getElementsByTagName("MaximumHeartRateBpm")[0];
-  const docMaximumHeartRateBpmValue = docMaximumHeartRateBpm.getElementsByTagName("Value")[0].childNodes[0].nodeValue;
-  const currTrack = doc.getElementsByTagName("Track")[0]
-  const currentTrackPoints = currTrack.getElementsByTagName("Trackpoint")
+function setLaps (doc) {
+  const docLaps = doc.getElementsByTagName("Lap")
 
-  xmlCombinedValues.value.totalTimeSeconds += parseFloat(docTotalTimeSeconds);
-  xmlCombinedValues.value.distanceMeters += parseFloat(docDistanceMeters);
-  xmlCombinedValues.value.maximumSpeed = xmlCombinedValues.value.maximumSpeed > parseFloat(docMaximumSpeed) ? xmlCombinedValues.value.maximumSpeed : parseFloat(docMaximumSpeed);
-  xmlCombinedValues.value.calories += parseFloat(docCalories);
-  xmlCombinedValues.value.maximumHeartRateBpm = xmlCombinedValues.value.maximumHeartRateBpm > parseFloat(docMaximumHeartRateBpmValue) ? xmlCombinedValues.value.maximumHeartRateBpm : parseFloat(docMaximumHeartRateBpmValue);
-  xmlCombinedValues.value.trackPoints = [
-    ...xmlCombinedValues.value.trackPoints,
-    ...currentTrackPoints
-  ];
+  allLabs.value.push(...docLaps)
+}
 
+function sortLapsByXmlStartTimeAttribute () {
+  allLabs.value.sort((a, b) => {
+    const aTime = new Date(a.getAttribute("StartTime"))
+    const bTime = new Date(b.getAttribute("StartTime"))
+    return aTime - bTime
+  })
 }
 
 function setXmlCombinedFileValues () {
@@ -99,29 +79,28 @@ function setXmlCombinedFileValues () {
   const rootXmlDoc = rootParser.parseFromString(combinedFile.value.content, "text/xml");
 
 
-  rootXmlDoc.getElementsByTagName("TotalTimeSeconds")[0].childNodes[0].nodeValue = xmlCombinedValues.value.totalTimeSeconds;
-  rootXmlDoc.getElementsByTagName("DistanceMeters")[0].childNodes[0].nodeValue = xmlCombinedValues.value.distanceMeters;
-  rootXmlDoc.getElementsByTagName("MaximumSpeed")[0].childNodes[0].nodeValue = xmlCombinedValues.value.maximumSpeed;
-  rootXmlDoc.getElementsByTagName("Calories")[0].childNodes[0].nodeValue = xmlCombinedValues.value.calories;
-  const rootXmlDocMaximumHeartRateBpm = rootXmlDoc.getElementsByTagName("MaximumHeartRateBpm")[0];
-  rootXmlDocMaximumHeartRateBpm.getElementsByTagName("Value")[0].childNodes[0].nodeValue = xmlCombinedValues.value.maximumHeartRateBpm;
+  const activities = rootXmlDoc.getElementsByTagName("Activity")[0]
 
-  const track = document.createElementNS('', "Track");
-  xmlCombinedValues.value.trackPoints
-    .sort((a, b) => {
-      const dateA = new Date(a.getElementsByTagName("Time")[0].childNodes[0].nodeValue);
-      const dateB = new Date(b.getElementsByTagName("Time")[0].childNodes[0].nodeValue);
-      return dateA - dateB;
-    })
-    .forEach((trackPoint) => {
-      track.appendChild(trackPoint)
-    })
+  removeCurrentLapEntriesInActivities(activities)
 
-  rootXmlDoc.getElementsByTagName("Track")[0].replaceWith(track);
+  addAllLapsToActivities(activities)
 
   const parsedXmlElement = new XMLSerializer().serializeToString(rootXmlDoc);
 
   combinedFile.value.content = parsedXmlElement
+}
+
+function removeCurrentLapEntriesInActivities(activities) {
+  const laps = activities.getElementsByTagName("Lap")
+  for (let i = laps.length - 1; i >= 0; i--) {
+    laps[i].parentNode.removeChild(laps[i])
+  }
+}
+
+function addAllLapsToActivities (activities) {
+  allLabs.value.forEach((lap) => {
+    activities.appendChild(lap)
+  })
 }
 
 const localFilesAreLessThan2 = computed(() => {
@@ -146,15 +125,15 @@ function downloadCombinedFile () {
     <v-btn outlined
            color="grey darken-2"
            :disabled="localFilesAreLessThan2"
-           :loading="combining.value"
+           :loading="combining"
            @click="combineFiles"
            class="w-max-content">
       Kombinieren
     </v-btn>
-    <CombinedValues :combined-values="xmlCombinedValues" />
+    <!-- <CombinedValues :combined-values="xmlCombinedValues" /> -->
     <v-btn outlined
            color="grey darken-2"
-           :disabled="xmlCombinedValues.totalTimeSeconds < 1"
+           :disabled="allLabs.length < 1"
            @click="downloadCombinedFile"
            class="w-max-content">
       Download Kombiniertes File
